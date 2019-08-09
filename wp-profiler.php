6fb77a2e6e46a2a7637dc1ea546b6fe899a9daf3
Diazoxide <aaron.yor@gmail.com>
2019-08-09 11:00:44 +0400

    initial commit

---
+++ wp-profiler.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WordPress Profiler - Powerful WordPress Profiler and Debugging Tool
+ * Plugin URI:
+ * Description: Profile your WordPress website to find slow hooks and callbacks
+ * Version: 1.1
+ * Author: Novembit
+ * Author URI: 
+ * License: GPLv2
+ */
